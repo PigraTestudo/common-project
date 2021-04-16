8d39f98c011d9070dc6a9039720f89d0ef5a64ec
Ivan Orlov <48783292+PigraTestudo@users.noreply.github.com>
2021-04-17 01:27:19 +1000

    Update and rename VoiceOverHeadsAndTales.py to VoiceOverHeadsAndTails.py
    Исправил грамматическую ошибку

--- VoiceOverHeadsAndTails.py
+++ VoiceOverHeadsAndTails.py
@@ -3,24 +3,24 @@ import numpy as np
 import time
 
 heads_count = 0
-tales_count = 0
+tails_count = 0
 
-headsandtales = np.random.choice([0, 1], size=10, p=[0.5, 0.5])
+headsandtails = np.random.choice([0, 1], size=10, p=[0.5, 0.5])
 
 engine = pyttsx3.init()
 
-for i in range(len(headsandtales)):
-    if headsandtales[i] == 0:
+for i in range(len(headsandtails)):
+    if headsandtails[i] == 0:
         engine.say("Heads")
         heads_count += 1
     else:
-        engine.say("Tales")
-        tales_count += 1
+        engine.say("Tails")
+        tails_count += 1
 
 time_start = time.time()
 engine.runAndWait()
 time_taken = time.time() - time_start
 
-print(headsandtales, '\n')
-print("There are {} heads and {} tales".format(heads_count, tales_count))
+print(headsandtails, '\n')
+print("There are {} heads and {} tails".format(heads_count, tails_count))
 print("It took ~{} seconds to voiceover them all".format(int(time_taken)))
